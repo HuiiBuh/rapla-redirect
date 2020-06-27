6e4b456fb43a5be9cdc7ed945658c46f7d8e525e
HuiiBuh <nhaderer1@gmail.com>
2020-06-27 22:20:12 +0200

    General code improvements

--- js/theme.js
+++ js/theme.js
@@ -1,28 +1,32 @@
-
 // This function is called when the page is first loaded
 function initTheme() {
 
     // Swith to the dark theme if the user has dark mode enabled
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
         switchToDarkTheme();
         return;
     }
-    
+
     // Try to get the client theme setting
-    var theme = localStorage.getItem('theme');
+    let theme = localStorage.getItem("theme");
 
     // If there is none, this is their first visit
-    if (theme == null) {
-
+    if (!theme) {
         // Set the theme to light
-        localStorage.setItem('theme', 'light');
+        localStorage.setItem("theme", "light");
+        theme = "light";
     }
 
-    // Otherwise style like wished
-    if (theme === "dark") {
-        switchToDarkTheme();
-    } else if (theme === "light") {
-        switchToLightTheme();
+    switch (theme) {
+        case "dark":
+            switchToDarkTheme();
+            break;
+        case "light":
+            switchToLightTheme();
+            break;
+        default:
+            switchToLightTheme();
+            break;
     }
 }
 
@@ -31,47 +35,44 @@ function initTheme() {
  */
 function toggleTheme() {
 
-    var currentTheme = localStorage.getItem('theme');
+    const currentTheme = localStorage.getItem("theme");
 
     switch (currentTheme) {
         case "dark":
-            localStorage.setItem('theme', 'light');
+            localStorage.setItem("theme", "light");
             switchToLightTheme();
             break;
         case "light":
-            localStorage.setItem('theme', 'dark');
+            localStorage.setItem("theme", "dark");
             switchToDarkTheme();
             break;
         default:
-            localStorage.setItem('theme', 'light');
+            localStorage.setItem("theme", "light");
             switchToLightTheme();
             break;
-            
     }
 }
 
 function switchToLightTheme() {
 
     // Change button icon
-    var button = document.getElementById("themeSwitchButton");
-    button.className = 'fas fa-moon my-float';
+    const button = document.getElementById("themeSwitchButton");
+    button.className = "fas fa-moon my-float";
 
     // Change stylesheets
     document.getElementById("css-light").rel = "stylesheet";
     document.getElementById("css-dark").rel = "stylesheet alternate";
-
 }
 
 function switchToDarkTheme() {
 
     // Change button icon
-    var button = document.getElementById("themeSwitchButton");
-    button.className = 'far fa-moon my-float';
+    const button = document.getElementById("themeSwitchButton");
+    button.className = "far fa-moon my-float";
 
     // Change stylesheets
     document.getElementById("css-dark").rel = "stylesheet";
     document.getElementById("css-light").rel = "stylesheet alternate";
-
 }
 
 initTheme();
